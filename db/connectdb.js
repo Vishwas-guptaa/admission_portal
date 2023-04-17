9ae4f8abc63e575e90d33b05b529f18d83e1ba19
Vishwas-guptaa <vishwasgupta81779@gmail.com>
2023-04-17 16:13:22 +0530

    admiss

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,10 +1,9 @@
 const mongoose  = require('mongoose')
 const url = "mongodb://127.0.0.1:27017/admissionportal"
-
-
+const live_Url ="mongodb+srv://vishwasgupta81779:vishwas143@cluster0.hdd68fy.mongodb.net/Brokerportal?retryWrites=true&w=majority"
 
 const connectDB =()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_Url)
 
 
     .then(()=>{
